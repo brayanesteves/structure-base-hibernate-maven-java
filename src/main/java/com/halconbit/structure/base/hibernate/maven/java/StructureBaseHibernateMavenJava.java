c67089e356c109c29ccb01c387bcc0f02fadfb24
Brayan Esteves <brayan.esteves93@gmail.com>
2023-11-28 17:27:26 -0400

    Struture Base [28/11/2023]

---
+++ src/main/java/com/halconbit/structure/base/hibernate/maven/java/StructureBaseHibernateMavenJava.java
@@ -0,0 +1,12 @@
+package com.halconbit.structure.base.hibernate.maven.java;
+
+/**
+ * @author Brayan Esteves (Venezuela)
+ * Email: brayan.esteves93@gmail.com
+ */
+public class StructureBaseHibernateMavenJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
